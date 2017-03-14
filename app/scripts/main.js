c8241542cca58f5cd914d57cf220fd0a969b3ed7
gsalisi <geoffreysalisi@gmail.com>
2017-03-13 20:18:25 -0400

    improved ui

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,23 +12,47 @@ $(document).ready(function() {
         $finalResultBlock.text(str);
     }
 
-    function showFinalResultCard() {
-        $result.append(`
-            <div class="card-panel">
-                <label>Final Phrase/Sentence:</label> 
-                <blockquote class="js-ths-finalresult-text">
+    function generateSynonymCardHeader() {
+        $result.append(
+            `<div class="js-ths-synonym-card-header">
+                <h4>Synonym suggestions:</h4>
+                <p>Click on the synonyms to add to thesaurus-ized result.</p>
+            </div>`);
+    }
 
+    function generateFinalTextCard() {
+        $result.append(`
+            <div class="js-ths-finalcard-panel card-panel">
+                <label class="js-ths-copy-label">Thesaurus-ize Phrase/Sentence:</label> 
+                <blockquote class="js-ths-finalresult-text">
                 </blockquote>
+                <a class="btn-floating blue ths-copy js-ths-copy" title="Copy to clipboard"><i class="fa fa-clipboard" aria-hidden="true"></i></a>
             </div>
+            </br>
         `)
+
+        $('.js-ths-copy').click(() => {
+            copyTextToClipboard($('.js-ths-finalresult-text').text().trim());
+            $('.js-ths-finalcard-panel').append('<span class="js-ths-copied-badge">Copied</span>');
+            window.setTimeout(() => {
+                $('.js-ths-copied-badge').remove();
+            }, 2000);
+        });
     }
 
     function generateWordCard(word, results) {
-        results.push(word);
-        const rMarkup = results.map(r => {
+        results.unshift(word);
+        const rMarkup = results.map((r, i) => {
+            const tmp = word.charAt(0);
+            if (word[0] === word[0].toUpperCase()
+                && word[0] !== word[0].toLowerCase()) {
+                r = r.charAt(0).toUpperCase() + r.slice(1);;
+            } else if (i === results.length-1) {
+                r += '.';
+            }
             return `
                 <div class="collection-item ths-wordcard-synonym js-ths-wordcard-synonym">
-                    ${r}
+                    <i class="tiny material-icons">add</i> <span class="synonym">${r}</span>
                 </div>
             `;
         }).join('\n');
@@ -42,10 +66,23 @@ $(document).ready(function() {
             </div>
         `);
 
-        $result.find(`.js-ths-wordcard-${word}`).click((elem) => {
-            addToFinalResult($(elem.target).text());
-            $(elem.target.parentElement.parentElement).remove();
-        })
+        // Attach listener
+        $result.find(`.js-ths-wordcard-${word} .js-ths-wordcard-synonym`).click(function (elem) {
+            const $targetElem = $(elem.target);
+            if ($targetElem.hasClass('js-ths-wordcard-synonym')) {
+                addToFinalResult($targetElem.find('.synonym').text().trim());
+                const $parent = $(`.js-ths-wordcard-${word}`);
+                $parent.remove();
+            } else if($targetElem.hasClass('synonym')) {
+                addToFinalResult($targetElem.text().trim());
+                const $parent = $(`.js-ths-wordcard-${word}`);
+                $parent.remove();
+            }
+
+            if(!$('.js-ths-wordcard-synonym').length) {
+                $('.js-ths-synonym-card-header').remove();
+            }
+        });
     }
 
     function launchRequests(words, i) {
@@ -69,7 +106,9 @@ $(document).ready(function() {
             }).filter(w => w.length);
 
             if (words && words.length) {
-                showFinalResultCard();
+                generateFinalTextCard();
+                generateSynonymCardHeader();
+
                 if (DEBUG) {
                     generateWordCard('Test', ['bad-mannered','ill-mannered','unmannered','unmannerly','natural','raw']);
                     generateWordCard('Lorem', ['uncivil','crude','primitive','bounderish','ill-bred','lowbred','underbred','yokelish']);
@@ -79,4 +118,39 @@ $(document).ready(function() {
             }
         }
     });
+
+    function copyTextToClipboard(text) {
+        var textArea = document.createElement('textarea');
+
+        // Place in top-left corner of screen regardless of scroll position.
+        textArea.style.position = 'fixed';
+        textArea.style.top = 0;
+        textArea.style.left = 0;
+
+        // Ensure it has a small width and height. Setting to 1px / 1em
+        // doesn't work as this gives a negative w/h on some browsers.
+        textArea.style.width = '2em';
+        textArea.style.height = '2em';
+
+        // We don't need padding, reducing the size if it does flash render.
+        textArea.style.padding = 0;
+
+        // Clean up any borders.
+        textArea.style.border = 'none';
+        textArea.style.outline = 'none';
+        textArea.style.boxShadow = 'none';
+        // Avoid flash of white box if rendered for any reason.
+        textArea.style.background = 'transparent';
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+
+        try {
+            const successful = document.execCommand('copy');
+        } catch (err) {
+            console.log('Oops, unable to copy');
+        }
+
+        document.body.removeChild(textArea);
+    }
 });
--- index.js
+++ index.js
@@ -45,54 +45,6 @@ app.get('/synonyms/:word', (req, res) => {
   });
 });
 
-app.get('/convert', (req, res) => {
-    if (!req.query.q) {
-        res.send(`Needs 'q' query parameter.`);
-    }
-
-    const words = req.query.q.split(' ').map(input => {
-      return input.replace(/[^a-zA-Z]/gi, '');
-    })
-    // console.log(words);
-    const promises = words.map(word => {
-      if (NOT_TO_THESAURIZE_SET.has(word.toUpperCase())) {
-        // console.log(word);
-        return q.when(word);
-      }
-      const requestOptions = {
-        url: `https://wordsapiv1.p.mashape.com/words/${word}`,
-        headers: {
-          'X-Mashape-Key': 'bLSnOP5JqXmshpIPnzCEUuqDHvMcp17TGREjsnir5BVQs9k6vg',
-          'Accept': 'application/json'
-        }
-      };
-      // console.log('promise');
-      return request(requestOptions).then((b) => {
-        const wordResults = JSON.parse(b).results;
-        if (wordResults) {
-          for (var i = 0; i < wordResults.length; i++) {
-            const wordRes = wordResults[i];
-            if (wordRes.partOfSpeech === 'adjective' || 
-              wordRes.partOfSpeech === 'verb' || 
-              wordRes.partOfSpeech === 'adverb') {
-              // console.log(wordRes);
-              const synonyms = wordRes.synonyms;
-              const r = getRandomArbitrary(0, synonyms.length);
-              return synonyms[r] || word;
-            }
-          }
-        }
-        return word;
-      })
-    })
-    
-    q.all(promises).then((bodies) => {
-      res.send(bodies.join(' ')+'.');
-    }).catch(err => console.log(err));
-
-
-});
-
 app.listen(app.get('port'),() => {
   console.log('Node app is running on port', app.get('port'));
 });
